7cb594f537877af54dcc981166a38f0323ccbb15
chidionyema <377396+chidionyema@users.noreply.github.com>
2026-08-25 03:24:20 +0100

    oci: policy statements reconciled from the OKE docs list plus inspect limits; login drops the key marker the tofu provider rejects; kubernetes_version v1.34.10

--- platform/oci/variables.tf
+++ platform/oci/variables.tf
@@ -41,5 +41,5 @@ variable "worker_memory_gb" {
 
 variable "kubernetes_version" {
   type    = string
-  default = "v1.31.1"
+  default = "v1.34.10"   # from `oci ce cluster-options get --cluster-option-id all`, 2026-08-25; v1.31 is no longer offered
 }
